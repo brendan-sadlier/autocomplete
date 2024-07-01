28d952a968025e2e9a7c3b5837dcd7f263d68ff3
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-07-01 13:58:55 +0100

    GREEN: Artist  model implemented and test passed

--- backend/src/main/java/com/autocomplete/model/Artist.java
+++ backend/src/main/java/com/autocomplete/model/Artist.java
@@ -1,4 +1,25 @@
 package com.autocomplete.model;
 
+import java.util.List;
+
 public class Artist {
+
+    private String name;
+    private List<Album> albums;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<Album> albums) {
+        this.albums = albums;
+    }
 }
