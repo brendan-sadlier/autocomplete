a45050daa6007208ea2a0e831ebfb3441b808dc6
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-07-01 13:54:47 +0100

    GREEN: Song model implemented and test passed

--- backend/src/main/java/com/autocomplete/model/Song.java
+++ backend/src/main/java/com/autocomplete/model/Song.java
@@ -1,4 +1,41 @@
 package com.autocomplete.model;
 
 public class Song {
+
+    private String title;
+    private String length;
+    private String artist;
+    private String album;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLength() {
+        return length;
+    }
+
+    public void setLength(String length) {
+        this.length = length;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
 }
