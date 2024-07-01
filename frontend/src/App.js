b390f4fb61e0542f2e9e6047bb10aef64324c9f1
brendan-sadlier <brendan.sadlier@ucdconnect.ie>
2024-07-01 14:36:38 +0100

    Implemented Dark/Light Mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme(theme => theme === 'light' ? 'dark' : 'light'); 
+  }
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    document.body.className = theme;
+  }, [theme]);
+
   return (
-    <div className="App">
-      <h1>Music Search</h1>
+    <div className="App search-container">
+      <div className='header'>
+        <h1 className='page-title'>Music <span>Search</span></h1>
+        <button className='theme-toggle-button' onClick={toggleTheme}>
+          {theme === 'light' ? '🌙' : '☀️'}
+        </button>
+      </div>
     </div>
   );
 }
